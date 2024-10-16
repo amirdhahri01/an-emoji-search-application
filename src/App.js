2a0d4648792708fdda8d827a64199bfa7a318135
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-16 23:30:31 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import EmojiResults from "./Dashboard/Emojis/EmojiResults";
 import Header from "./Dashboard/Header/Header";
 import SearchBar from "./Dashboard/Searchbar/SearchBar";
+import Filter from "./Dashboard/Emojis/Filter";
 class App extends PureComponent {
+  state = {
+    filteredEmojis: Filter("", 20),
+  };
   render() {
     return (
       <div>
