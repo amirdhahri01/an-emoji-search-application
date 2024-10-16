18d74bc03b5923ad75628844b125ece1539fd244
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-16 23:56:55 +0100

    The EmojiResultRow component is done

--- src/Dashboard/Emojis/EmojiResultRow.jsx
+++ src/Dashboard/Emojis/EmojiResultRow.jsx
@@ -1,7 +1,17 @@
 import { PureComponent } from "react";
 class EmojiResultRow extends PureComponent {
   render() {
-    return <></>;
+    const [title, symbol] = this.props;
+    // const codePointHex = symbol.codePointAt(0).toString(16);
+    return (
+      <>
+        <div>
+          <img src="" alt={title} />
+          <span className="title">{title}</span>
+          <span className="info">Click to Copy {symbol}</span>
+        </div>
+      </>
+    );
   }
 }
 
