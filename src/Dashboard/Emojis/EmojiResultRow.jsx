dd7cfdb01b0ff1933b072a6cd3b5dbf08d5b4c31
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-17 00:15:50 +0100

    An update

--- src/Dashboard/Emojis/EmojiResultRow.jsx
+++ src/Dashboard/Emojis/EmojiResultRow.jsx
@@ -1,10 +1,11 @@
 import { PureComponent } from "react";
+import "./EmojiResultRow.css";
 class EmojiResultRow extends PureComponent {
   render() {
     const { title, symbol } = this.props;
     return (
       <>
-        <div>
+        <div className="component-emoji-result-row">
           <span className="symbol">{symbol}</span>
           <span className="title">{title}</span>
           <span className="info">Click to Copy </span>
